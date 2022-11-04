f5e6d0cf791efed95f168b7aac5a61fd5e112d06
Da1W <denissalyhov@gmail.com>
2022-11-04 20:37:01 +0500

    Smooth lerp between new and previous camera offset/scale

--- src/To Valhalla/Assets/Scripts/View/VelocityHandler.cs
+++ src/To Valhalla/Assets/Scripts/View/VelocityHandler.cs
@@ -15,6 +15,15 @@ namespace View
         [SerializeField] private float _maxSpeedCameraScale;
         [SerializeField] private float _positiveSpeedCameraOffset;
         [SerializeField] private float _negativeSpeedCameraOffset;
+        [SerializeField] [Range(0, 0.2f)] private float _cameraOffsetSmoothSpeed;
+        [SerializeField] [Range(0, 0.2f)] private float _cameraScaleSmoothSpeed;
+
+        private CinemachineFramingTransposer _cinemachineFramingTransposer;
+
+        private void Awake()
+        {
+            _cinemachineFramingTransposer = _camera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        }
 
         private void OnEnable()
         {
@@ -25,21 +34,23 @@ namespace View
         {
             SetCameraSize(_camera, _baseSize,
                 Mathf.Lerp(_minSpeedCameraScale, _maxSpeedCameraScale, Mathf.Clamp01(velocity / _maxSpeed)));
-            
-            // SetCameraOffsetY(_camera,
-            //     Mathf.Lerp(_negativeSpeedCameraOffset, _positiveSpeedCameraOffset,
-            //         Mathf.InverseLerp(-_maxVerticalSpeed, _maxVerticalSpeed, verticalVelocity)));
+
+            SetCameraOffsetY(_cinemachineFramingTransposer,
+                Mathf.Lerp(_negativeSpeedCameraOffset, _positiveSpeedCameraOffset,
+                    Mathf.InverseLerp(-_maxVerticalSpeed, _maxVerticalSpeed, verticalVelocity)));
         }
 
         private void SetCameraSize(CinemachineVirtualCamera virtualCamera, float baseSize, float scale)
         {
-            virtualCamera.m_Lens.OrthographicSize = baseSize * scale;
+            float newSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, baseSize * scale, _cameraScaleSmoothSpeed);
+            virtualCamera.m_Lens.OrthographicSize = newSize;
         }
 
-        private void SetCameraOffsetY(CinemachineVirtualCamera virtualCamera, float offset)
+        private void SetCameraOffsetY(CinemachineFramingTransposer framingTransposer, float offset)
         {
-            virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>().m_TrackedObjectOffset =
-                new Vector3(0, offset, 0);
+            float newY = Mathf.Lerp(framingTransposer.m_TrackedObjectOffset.y, offset, _cameraOffsetSmoothSpeed);
+
+            framingTransposer.m_TrackedObjectOffset = new Vector3(0, newY, 0);
         }
 
         private void OnDisable()
