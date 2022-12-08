bf7f6dfd72d07069a3d30d181d056862701b2688
Da1W <denissalyhov@gmail.com>
2022-12-09 01:41:53 +0500

    Parallax + bg

---
+++ src/To Valhalla/Assets/Scripts/View/BGHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+public class BGHandler : MonoBehaviour
+{
+    [SerializeField] private PlayerTransformController _playerTransform;
+    [SerializeField] private GameObject _bgLeft;
+    [SerializeField] private GameObject _bgCenter;
+    [SerializeField] private GameObject _bgRight;
+    [SerializeField] private float _bgHalfSizeX;
+
+    private float _center = 0.0f;
+
+    private void Update()
+    {
+        CheckToSwitchBGPositions();
+    }
+
+    private void CheckToSwitchBGPositions()
+    {
+        if (_playerTransform.GetPosition().x > _bgCenter.transform.position.x + _bgHalfSizeX)
+        {
+            _bgLeft.transform.position += new Vector3(_bgHalfSizeX * 6, 0, 0);
+            (_bgRight, _bgLeft, _bgCenter) = (_bgLeft, _bgCenter, _bgRight);
+        }
+        else if (_playerTransform.GetPosition().x < _bgCenter.transform.position.x - _bgHalfSizeX)
+        {
+            _bgRight.transform.position -= new Vector3(_bgHalfSizeX * 6, 0, 0);
+            (_bgCenter, _bgRight, _bgLeft) = (_bgLeft, _bgCenter, _bgRight);
+        }
+    }
+}
---
+++ src/To Valhalla/Assets/Scripts/View/ParallaxLayers.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxLayers : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _layers = new List<Transform>();
+    [SerializeField] private List<Vector2> _effectMultiplicators = new List<Vector2>();
+
+    private Transform _cameraTransform;
+    private Vector3 _previousCameraPos;
+
+    private void Start()
+    {
+        _cameraTransform = Camera.main.transform;
+        _previousCameraPos = _cameraTransform.position;
+    }
+
+    private void Update()
+    {
+        if (_cameraTransform.position - _previousCameraPos == Vector3.zero)
+            return;
+
+        for(int i = 0; i < _layers.Count; i++)
+        {
+            Vector3 delta = _cameraTransform.position - _previousCameraPos;
+            _layers[i].position += new Vector3(delta.x * _effectMultiplicators[i].x, delta.y * _effectMultiplicators[i].y, 0);
+        }
+
+        _previousCameraPos = _cameraTransform.position;
+    }
+}
